97503541b2b45002b6d6629a194247c40d355088
amaniiej <amanuel22e@gmail.com>
2026-01-02 22:58:02 +0300

    ws

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -3,28 +3,32 @@ import { cookies } from 'next/headers';
 
 import { type CookieOptions, createServerClient } from '@supabase/ssr';
 
-export const createClient = () => {
-    const cookieStore = cookies();
+export async function createClient() {
+    const cookieStore = await cookies();
 
     return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
         cookies: {
-            get(name: string) {
-                return cookieStore.get(name)?.value;
+            async get(name: string) {
+                return (await cookieStore).get(name)?.value;
             },
-            set(name: string, value: string, options: CookieOptions) {
+            async set(name: string, value: string, options: CookieOptions) {
                 try {
-                    cookieStore.set({ name, value, ...options });
+                    (await cookieStore).set({ name, value, ...options });
                 } catch (error) {
-                    // Handle error if needed
+                    // The `set` method was called from a Server Component.
+                    // This can be ignored if you have middleware refreshing
+                    // user sessions.
                 }
             },
-            remove(name: string, options: CookieOptions) {
+            async remove(name: string, options: CookieOptions) {
                 try {
-                    cookieStore.set({ name, value: '', ...options });
+                    (await cookieStore).set({ name, value: '', ...options });
                 } catch (error) {
-                    // Handle error if needed
+                    // The `delete` method was called from a Server Component.
+                    // This can be ignored if you have middleware refreshing
+                    // user sessions.
                 }
             }
         }
     });
-};
+}
